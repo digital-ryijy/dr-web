e95852577c8c88b7949c624979e2089795694752
Varvara Kronberg <github@varvara.ca>
2021-06-08 14:54:13 -0700

    testing first animation with gsap and react class components

--- src/AnimateLogo.js
+++ src/AnimateLogo.js
@@ -1,14 +1,55 @@
 import logo from './images/updated_logo_bold_white.svg';
+import React, { Component } from 'react';
+/*global gsap:true */
+class AnimatedLogo extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        // logo container
+
+        this.logoContainer = null;
+
+        // logo tween
+
+        this.logoTween = null;
+
+    }
 
 
-{/* <img src={logo} className="logo" alt="ryijy logo" /> */ }
 
-function AnimatedLogo() {
-    return (
-        <div className="homepage-logo-container">
-            <img src={logo} className="logo" alt="ryijy logo" />
-        </div>
-    );
+    componentDidMount() {
+
+        // create logo tween
+
+        this.logoTween =
+
+            gsap.from(this.logoContainer, { duration: 3, x: '-100vw', opacity: 0 });
+
+
+    }
+
+
+
+    render() {
+
+        return (
+
+            <div className="homepage-logo-container">
+
+                <img src={logo} className="logo" alt="ryijy logo" ref={img => this.logoContainer = img} />
+
+
+
+            </div>
+
+        );
+
+    }
+
 }
+
+
 
 export default AnimatedLogo;
